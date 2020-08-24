67ed53789b1c47861a32793538912db2764e9692
Phong Duong <phongduong1710@gmail.com>
2020-08-24 21:18:59 +0700

    Update docs

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,28 @@
-const { src, dest } = require("gulp");
+const { src, dest, series, watch } = require("gulp");
 const cleanCSS = require("gulp-clean-css");
 const rename = require("gulp-rename");
 
-const buildCSS = () => {
-  return src("src/vn.css")
-    .pipe(dest("dist"))
+const buildVNCSS = () => {
+  return src("src/styles/vn.css")
+    .pipe(dest("dist/styles"))
     .pipe(cleanCSS())
     .pipe(rename({ extname: ".min.css" }))
-    .pipe(dest("dist"));
+    .pipe(dest("dist/styles"));
 };
 
-exports.default = buildCSS;
+const buildCSS = () => {
+  return src(["src/styles/*.css", "!src/styles/vn.css"]).pipe(
+    dest("dist/styles")
+  );
+};
+
+const watchCSS = () => {
+  watch("src/styles/*.css", buildCSS);
+};
+
+const copyImage = () => {
+  return src("src/image/*.png").pipe(dest("dist/image"));
+};
+
+exports.default = series([copyImage, buildCSS, buildVNCSS, watchCSS]);
+exports.build = series([copyImage, buildCSS, buildVNCSS]);
